eb69c17f42d15c61008e4f4c8a988fa7b8397be2
patrick-kuenig <kuenig.patrick@gmail.com>
2024-10-20 21:37:41 +0300

    homework_20_10_2024

---
+++ Urban19/task1/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=5, decimal_places=1)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer)
+
