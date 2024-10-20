710bc5f9706783c086f01a8cd14425856036a261
patrick-kuenig <kuenig.patrick@gmail.com>
2024-10-20 22:24:29 +0300

    homework_20_10_2024

--- Urban19/task1/models.py
+++ Urban19/task1/models.py
@@ -7,6 +7,9 @@ class Buyer(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Game(models.Model):
     title = models.CharField(max_length=255)
@@ -15,4 +18,7 @@ class Game(models.Model):
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer)
+
+    def __str__(self):
+        return self.title
 
